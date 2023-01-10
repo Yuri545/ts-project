386d14fe2fe0c013c74237fae42ae6aa5de7697a
YuriTur <utur2006@gmail.com>
2023-01-10 14:08:32 +0200

    CW #31 +

--- app.js
+++ app.js
@@ -1,44 +1,11 @@
 "use strict";
-const aCodeAscii = 'a'.charCodeAt(0);
-const zCodeAscii = 'z'.charCodeAt(0);
-const nEnglishLetters = zCodeAscii - aCodeAscii + 1;
-function shiftCipher(str, shift = 1) {
-    return cipherDecipher(str, shift, mapperCipher);
-}
-function shiftDecipher(str, shift = 1) {
-    return cipherDecipher(str, shift, mapperDecipher);
-}
-function cipherDecipher(str, shift, mapperFun) {
-    //const arStr: string[] = Array.from(str);
-    const arStr = Array.from(str);
-    const arRes = arStr.map(symb => {
-        let res = symb;
-        if (symb <= 'z' && symb >= 'a') {
-            res = mapperFun(symb, shift);
-        }
-        return res;
-    });
-    return arRes.join('');
-}
-function mapperCipher(symb, shift) {
-    const actualShift = (symb.charCodeAt(0) - aCodeAscii + shift) % nEnglishLetters;
-    return String.fromCharCode(aCodeAscii + actualShift);
-}
-function mapperDecipher(symb, shift) {
-    const actualShift = (zCodeAscii - symb.charCodeAt(0) + shift) % nEnglishLetters;
-    return String.fromCharCode(zCodeAscii - actualShift);
-}
-function testCipherDecipher(data, testName) {
-    console.log(`${"*".repeat(10)}${testName}${"*".repeat(10)}`);
-    const funForTest = testName === "cipherTest" ? shiftCipher : shiftDecipher;
-    data.forEach((obj => console.log(`str=${obj.str}, shift=${obj.shift || 1} => ${funForTest(obj.str, obj.shift)}`)));
-}
-const dataForCipherTest = [
-    { str: "abc" }, { str: "abz", shift: 1000 }
+Object.defineProperty(exports, "__esModule", { value: true });
+const Rectangle_1 = require("./Rectangle");
+const Square_1 = require("./Square");
+const shapes = [
+    new Rectangle_1.Rectangle(3, 4),
+    new Square_1.Square(5)
 ];
-testCipherDecipher(dataForCipherTest, "cipherTest");
-const dataForDecipherTest = [
-    { str: "bcd" }, { str: "mnl", shift: 1000 }
-];
-testCipherDecipher(dataForDecipherTest, "decipherTest");
+const sumOfShapes = shapes.reduce((res, cur) => res + cur.getSquare(), 0);
+console.log(sumOfShapes);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,51 +1,9 @@
-const aCodeAscii: number = 'a'.charCodeAt(0);
-const zCodeAscii: number = 'z'.charCodeAt(0);
-const nEnglishLetters: number = zCodeAscii - aCodeAscii + 1;
- 
-function shiftCipher(str: string, shift: number = 1): string {
-    return cipherDecipher(str, shift, mapperCipher);
-}
-function shiftDecipher(str: string, shift: number = 1) : string {
-    return cipherDecipher(str, shift, mapperDecipher);
-}
-type MapperFunction = (symb: string, shift: number) => string;
-function cipherDecipher(str: string, shift: number,
-     mapperFun: MapperFunction): string {
-        //const arStr: string[] = Array.from(str);
-        const arStr: Array<string> = Array.from(str);
-        const arRes: Array<string> = arStr.map(symb => {
-            let res: string = symb;
-            if (symb <= 'z' && symb >= 'a') {
-                res = mapperFun(symb, shift);
-            }
-            return res;
-        })
-        return arRes.join('');
-     }
-     function mapperCipher(symb: string, shift: number) : string {
-        const actualShift: number = (symb.charCodeAt(0) - aCodeAscii + shift) % nEnglishLetters;
-        return String.fromCharCode(aCodeAscii + actualShift);
-     }
-     function mapperDecipher(symb: string, shift: number): string {
-        const actualShift: number = (zCodeAscii - symb.charCodeAt(0) + shift) % nEnglishLetters;
-        return String.fromCharCode(zCodeAscii - actualShift);
-     }
-     type TestObj = {
-        str: string,
-         shift?: number
-     }
-     function testCipherDecipher(data: Array<TestObj>,
-         testName: string): void{
-            console.log(`${"*".repeat(10)}${testName}${"*".repeat(10)}`)
-            const funForTest: MapperFunction
-             = testName === "cipherTest" ? shiftCipher : shiftDecipher;
-data.forEach((obj => console.log(`str=${obj.str}, shift=${obj.shift || 1} => ${funForTest(obj.str, obj.shift)}`))) 
-         }
-         const dataForCipherTest: Array<TestObj> = [
-            {str: "abc"}, {str: "abz", shift: 1000}
-         ];
-         testCipherDecipher(dataForCipherTest, "cipherTest");
-         const dataForDecipherTest: Array<TestObj> = [
-            {str: "bcd"}, {str: "mnl", shift: 1000}
-         ];
-         testCipherDecipher(dataForDecipherTest, "decipherTest");
+import { Shape } from "./ShapeInterface";
+import { Rectangle } from "./Rectangle";
+import { Square } from "./Square";
+const shapes: Array<Shape> = [
+    new Rectangle(3, 4),
+    new Square(5)
+];
+const sumOfShapes: number = shapes.reduce((res, cur) => res + cur.getSquare(), 0)
+console.log(sumOfShapes)
