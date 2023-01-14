384c446c44f0f874e1ef8f72b98aa7bd8d0512b5
YuriTur <utur2006@gmail.com>
2023-01-14 10:29:51 +0200

    CW #32 OOP Employee WageEmployee

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 "use strict";
+// import { Shape } from "./ShapeInterface";
+// import { Rectangle } from "./Rectangle";
+// import { Square } from "./Square";
+// import { CipherDecipher } from "./CipherDecipher";
+// import { ShapesContainer } from "./ShapesContainer";
+// const shapes: ShapesContainer = new ShapesContainer ([
+//     new Rectangle(3, 4),
+//     new Square(5),
+//     new ShapesContainer([new ShapesContainer([new Square(10),new Square(4)]),
+//      new Rectangle(6, 4)])
+// ]);
 Object.defineProperty(exports, "__esModule", { value: true });
-const Rectangle_1 = require("./Rectangle");
-const Square_1 = require("./Square");
-const ShapesContainer_1 = require("./ShapesContainer");
-const shapes = new ShapesContainer_1.ShapesContainer([
-    new Rectangle_1.Rectangle(3, 4),
-    new Square_1.Square(5),
-    new ShapesContainer_1.ShapesContainer([new ShapesContainer_1.ShapesContainer([new Square_1.Square(10), new Square_1.Square(4)]),
-        new Rectangle_1.Rectangle(6, 4)])
-]);
-const totalSquare = shapes.getSquare();
-console.log(totalSquare);
-// const cipherDecipher: CipherDecipher = new CipherDecipher(32, 127, 10);
-// console.log(cipherDecipher.cipher("Hello World!"));
-// console.log(cipherDecipher.decipher("Rovvy*ay|vn+"));
+const WageEmployee_1 = require("./WageEmployee");
+// const totalSquare: number = shapes.getSquare();
+// console.log(totalSquare)
+// // const cipherDecipher: CipherDecipher = new CipherDecipher(32, 127, 10);
+// // console.log(cipherDecipher.cipher("Hello World!"));
+// // console.log(cipherDecipher.decipher("Rovvy*ay|vn+"));
+const employee = new WageEmployee_1.WageEmployee(123, "Vasya", 2000, "QA", 10000, 100, 50);
+console.log(employee.id);
+employee.basicSalary = 60000;
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,18 +1,24 @@
-import { Shape } from "./ShapeInterface";
-import { Rectangle } from "./Rectangle";
-import { Square } from "./Square";
-import { CipherDecipher } from "./CipherDecipher";
-import { ShapesContainer } from "./ShapesContainer";
-const shapes: ShapesContainer = new ShapesContainer ([
-    new Rectangle(3, 4),
-    new Square(5),
-    new ShapesContainer([new ShapesContainer([new Square(10),new Square(4)]),
-     new Rectangle(6, 4)])
-]);
+// import { Shape } from "./ShapeInterface";
+// import { Rectangle } from "./Rectangle";
+// import { Square } from "./Square";
+// import { CipherDecipher } from "./CipherDecipher";
+// import { ShapesContainer } from "./ShapesContainer";
+// const shapes: ShapesContainer = new ShapesContainer ([
+//     new Rectangle(3, 4),
+//     new Square(5),
+//     new ShapesContainer([new ShapesContainer([new Square(10),new Square(4)]),
+//      new Rectangle(6, 4)])
+// ]);
 
-const totalSquare: number = shapes.getSquare();
-console.log(totalSquare)
-// const cipherDecipher: CipherDecipher = new CipherDecipher(32, 127, 10);
+import { WageEmployee } from "./WageEmployee";
 
-// console.log(cipherDecipher.cipher("Hello World!"));
-// console.log(cipherDecipher.decipher("Rovvy*ay|vn+"));
+// const totalSquare: number = shapes.getSquare();
+// console.log(totalSquare)
+// // const cipherDecipher: CipherDecipher = new CipherDecipher(32, 127, 10);
+
+// // console.log(cipherDecipher.cipher("Hello World!"));
+// // console.log(cipherDecipher.decipher("Rovvy*ay|vn+"));
+const employee: WageEmployee = new WageEmployee(123, "Vasya", 2000, "QA",
+ 10000, 100,50);
+ console.log(employee.id);
+ employee.basicSalary = 60000;
